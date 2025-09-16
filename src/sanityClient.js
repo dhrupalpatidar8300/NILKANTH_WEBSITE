037aee6a8f5da3e428d3949316f3e49e7d99a1a0
Dhrupal Patel <dhrupalpatidar1313@gmail.com>
2025-09-17 01:49:33 +0530

    Add .env variable

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,8 +1,8 @@
 import sanityClient from '@sanity/client';
 
 export default sanityClient({
-    projectId: 'id27d31g', // You can find this in sanity-studio/sanity.json
-    dataset: 'production',
+    projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+    dataset: import.meta.env.VITE_SANITY_DATASET,
     useCdn: true,
     apiVersion: '2021-03-25',
 });
