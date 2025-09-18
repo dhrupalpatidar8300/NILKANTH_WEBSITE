72b93fd4c842fd8efae24dd69e1871dfbd305275
Dhrupal Patel <dhrupalpatidar1313@gmail.com>
2025-09-18 10:23:11 +0530

    Refactor Navbar component to use Link from react-router-dom for internal navigation

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from './Navbar.module.css';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <nav className={styles.navbar}>
-            <a href="/" className={styles.brand}>Nilakanth ATM Services</a>
+            <Link to="/" className={styles.brand}>Nilakanth ATM Services</Link>
             <ul className={styles.navList}>
-                <li><a href="/" className={styles.navLink}>Home</a></li>
-                <li><a href="/services" className={styles.navLink}>Our Services</a></li>
-                <li><a href="/about" className={styles.navLink}>About</a></li>
-                <li><a href="/contact" className={styles.navLink}>Contact</a></li>
+                <li><Link to="/" className={styles.navLink}>Home</Link></li>
+                <li><Link to="/services" className={styles.navLink}>Our Services</Link></li>
+                <li><Link to="/about" className={styles.navLink}>About</Link></li>
+                <li><Link to="/contact" className={styles.navLink}>Contact</Link></li>
             </ul>
         </nav>
     );
